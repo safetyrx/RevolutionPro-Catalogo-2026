48c61c1a5036458403b82bd90006df5056dae0b9
safetyrx <bajoluzsatelital@gmail.com>
2026-09-15 09:57:42 -0300

    preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Exponemos una API segura al frontend
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Esta función invoca el canal 'save-pdf' del main_2.js
-    savePDF: (filename) => ipcRenderer.invoke('save-pdf', filename)
+  savePDF: () => ipcRenderer.invoke('save-pdf')
 });
